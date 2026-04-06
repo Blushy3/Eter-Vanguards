af2d00b04229e55b30ef2ceb0356e06ec3f5a233
Blushyk <107561690+Blushy3@users.noreply.github.com>
2026-04-07 00:54:43 +0200

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SB_URL = 'https://zllishknfqylpommgnew.supabase.co';
-const SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsbGlzaGtuZnF5bHBvbW1nbmV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzOTk4MDksImV4cCI6MjA5MDk3NTgwOX0.Tm7ZGifcT5Y2kBJxaTUK_ASQruVcpHrM9UDMtmQiT4A';
+const SB_URL = 'https://fzxjfwimunxuehxqglcm.supabase.co';
+const SB_KEY = 'sb_publishable_NIOtMGAQkjSw0rIcXZwWtg__RKkxjvP';
 
 export const supabase = createClient(SB_URL, SB_KEY);
